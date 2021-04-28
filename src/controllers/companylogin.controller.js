4e137d6877b2bf4e243e587ffdb142a96f79fef3
unknown <dipenprajapati1999@gmail.com>
2021-04-28 11:52:35 +0530

    second commit

--- src/controllers/companylogin.controller.js
+++ src/controllers/companylogin.controller.js
@@ -1,58 +1,57 @@
 const companyloginmodel = require('../models/companyloginmodel');
 const bcrypt = require('bcrypt');
 
-exports.complogin = function(req, res){
-    res.render('complogin', {SuccessMsg: req.flash('Success'), ErrorMsg: req.flash('Error'), title: 'Login'});
- }
+exports.complogin = function(req, res) {
+    res.render('complogin', { SuccessMsg: req.flash('Success'), ErrorMsg: req.flash('Error'), title: 'Login' });
+}
 
-exports.company = function(req,res){
+exports.company = function(req, res) {
     let encrpyt = " "
     const uname = req.body.username
     const password = req.body.password
     console.log(uname);
     console.log(password);
-    if(req.body == null){
-        req.flash("Error",'Please enter Username and Password!!');
+    if (req.body == null) {
+        req.flash("Error", 'Please enter Username and Password!!');
         res.redirect('/complogin')
     }
 
-    companyloginmodel.login(uname,function(error,data){
-        if(data == null){
+    companyloginmodel.login(uname, function(error, data) {
+        if (data == null) {
             req.flash('Error', 'Invalid Username or Password!!');
             res.redirect('/complogin');
-        }
-        else{
-            console.log('result',data)
-            req.session.user=data[0].cmp_id;
+        } else {
+            console.log('result', data)
+            req.session.user = data[0].cmp_id;
             req.session.usertype1 = 'company';
             const uname = data[0].username;
             const pwd = data[0].password;
             console.log(uname);
             console.log(pwd);
-            console.log('session id:',req.session.user);
-            console.log('usertype:',req.session.usertype1);
+            console.log('session id:', req.session.user);
+            console.log('usertype:', req.session.usertype1);
 
 
-            if(req.session.user == data[0].cmp_id){
-                bcrypt.compare(password,pwd).then((result=>{
-                    console.log('result:',result);
-                    if(result){
-                        req.flash('Success',`Welcome ${data[0].username}!!`);
-                        res.redirect('/company/home/'+ req.session.user);
-                    }
-                    else{
-                        req.flash('Error','Invalid Password!!')
+            if (req.session.user == data[0].cmp_id) {
+                bcrypt.compare(password, pwd).then((result => {
+                    console.log('result:', result);
+                    if (result) {
+                        req.flash('Success', `Welcome ${data[0].username}!!`);
+                        res.redirect('/company/home/' + req.session.user);
+                    } else {
+                        req.flash('Error', 'Invalid Password!!')
                         res.redirect('complogin');
                     }
-                })).catch((err)=>{
-                    console.error(err);
+                })).catch((err) => {
+                    //console.error(err);
+                    req.flash('Error', 'Invalid Password!!')
+                    res.redirect('complogin');
                 })
-            }
-            else{
-                req.flash('Error','Please Login First!!');
+            } else {
+                req.flash('Error', 'Please Login First!!');
                 res.redirect('complogin');
             }
         }
-            
+
     })
 }
--- src/controllers/companyregister.controller.js
+++ src/controllers/companyregister.controller.js
@@ -3,28 +3,28 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 
-exports.compregister = function(req, res){       
-    res.render('compregister', {SuccessMsg: req.flash('Success'), ErrorMsg: req.flash('Error'), title: 'compregister'});
-  }
+exports.compregister = function(req, res) {
+    res.render('compregister', { SuccessMsg: req.flash('Success'), ErrorMsg: req.flash('Error'), title: 'compregister' });
+}
 
-  exports.saveCompany = async function(req, res){
-      value = req.body;
-      
-    if(value!=null){
-      const encryptedPassword = await bcrypt.hash(req.body.password, saltRounds);
-      req.body.password = encryptedPassword;      
-      const data = new registercompany(value);   
-      data.password = encryptedPassword; 
-      console.log(value);
-      registercompany.newcompany(data, function(err,result){
-          console.log(result);
-        if(result.error){   
-          req.flash('Error', 'Something went wrong while adding user!');           
-          res.redirect('/compregister');            
-        }else{
-            req.flash('Success', 'User added successfully!');           
-            res.redirect('/complogin');  
-        }                                   
-      });        
-    } 
-  }
+exports.saveCompany = async function(req, res) {
+    value = req.body;
+
+    if (value != null) {
+        const encryptedPassword = await bcrypt.hash(req.body.password, saltRounds);
+        req.body.password = encryptedPassword;
+        const data = new registercompany(value);
+        data.password = encryptedPassword;
+        console.log(value);
+        registercompany.newcompany(data, function(err, result) {
+            console.log(result);
+            if (result.error) {
+                req.flash('Error', 'Something went wrong while adding user!');
+                res.redirect('/compregister');
+            } else {
+                req.flash('Success', 'User added successfully!');
+                res.redirect('/complogin');
+            }
+        });
+    }
+}
